ab5d7481800cb1dc7a60197ad847f16e4d01585b
Yahya LBARNOUSSI <yahya.lbarnoussi@gmail.com>
2019-11-28 17:41:09 +0100

    Added binaries

--- include/cell.hpp
+++ include/cell.hpp
@@ -1,4 +1,4 @@
-#include <SDL2/SDL.h>
+//#include <SDL2/SDL.h>
 
 #ifndef CELL_H
 #define CELL_H
--- include/cell_SDL.hpp
+++ include/cell_SDL.hpp
@@ -1,4 +1,5 @@
-#include <SDL2/SDL.h>
+//#include <SDL2/SDL.h>
+#include <SDL.h>
 #include <iostream>
 #include "cell.hpp" 
 #ifndef CELL_SDL_H
